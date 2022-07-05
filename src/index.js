4bc6daf83a47a95d62a59b88233f21f32cb45e5d
wayungi <fwayungi@gmail.com>
2022-07-05 19:44:12 +0300

    Link css file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
   return element;
 }
 
