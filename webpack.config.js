92510685034d0d1ec142f4ad8e531649c9140e29
wayungi <fwayungi@gmail.com>
2022-07-05 19:27:19 +0300

    Add html-webpack-plugin configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
