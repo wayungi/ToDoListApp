a6a6a4350a34e9d592a55c4d0e9537bbd24ca16b
wayungi <fwayungi@gmail.com>
2022-07-05 20:22:01 +0300

    remove test code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
-}
-
-document.body.appendChild(component());
