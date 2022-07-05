9b29767788a5bb37e3cbee635727818b295b5e8d
wayungi <fwayungi@gmail.com>
2022-07-05 23:12:15 +0300

    Add styles to wrapper div

--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,8 @@ const createCheckBox = () => {
   const checkbox = document.createElement('input');
   checkbox.classList.add('cb');
   checkbox.type = 'checkbox';
-  checkbox.name = 'name';
+  checkbox.classList.add('check_box');
+  
   /*
     TODO: add event listener here
   */
@@ -13,12 +14,14 @@ const addActivity = (TodoListElement, task) => {
   const taskBlock = document.createElement('div');
   taskBlock.classList.add('task_block');
 
+  // more icon holder dive
   const dots = document.createElement('div');
   dots.classList.add('more');
+  dots.textContent = 'w';
 
   // div to group checkbox & description
   const itemGroup = document.createElement('div');
-  itemGroup.classList.add('cb_desc');
+  itemGroup.classList.add('cb_desc', 'flex-row');
 
   const p = document.createElement('p');
   p.textContent = task.description;
@@ -27,8 +30,10 @@ const addActivity = (TodoListElement, task) => {
   itemGroup.appendChild(checkBox);
   itemGroup.appendChild(p);
 
-  TodoListElement.appendChild(itemGroup);
-  TodoListElement.appendChild(dots);
+  taskBlock.appendChild(itemGroup);
+  taskBlock.appendChild(dots);
+
+  TodoListElement.appendChild(taskBlock);
   return null;
 };
 
