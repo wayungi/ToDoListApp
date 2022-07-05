85ba5ada24d01db43f1d5c8f7e9c2e1aada4f842
wayungi <fwayungi@gmail.com>
2022-07-05 19:48:09 +0300

    Add dev-server config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
