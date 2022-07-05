829d23ce9637716a9f41d1b2c03a50c251c0fcfd
wayungi <fwayungi@gmail.com>
2022-07-06 00:58:36 +0300

    Add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ],
   },
