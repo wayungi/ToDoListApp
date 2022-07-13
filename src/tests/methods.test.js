436ef5f8aa73f81141025e2e9bab034824ba984f
wayungi <fwayungi@gmail.com>
2022-07-13 23:26:22 +0300

    Update .gitignore

---
+++ src/tests/methods.test.js
@@ -0,0 +1,42 @@
+import { removeTask, addTask, getTasks } from '../modules/methods.js';
+
+describe('addtask', () => {
+  // Arrange
+  const task1 = {
+    description: 'Play soccer',
+    completed: false,
+    isbn: '1657742044934',
+    id: '1',
+  };
+
+  const task2 = {
+    description: 'Eat food',
+    completed: false,
+    isbn: '1657742044935',
+    id: '2',
+  };
+
+  const task3 = {
+    description: 'Take a nap',
+    completed: false,
+    isbn: '1657742044936',
+    id: '3',
+  };
+
+  test('Should add 1 task', () => {
+    // Act
+    addTask(task1);
+
+    //  Assert
+    expect(getTasks().length).toEqual(1);
+  });
+
+  test('Should add 2 task', () => {
+    // Act
+    addTask(task2);
+    addTask(task3);
+
+    //  Assert
+    expect(getTasks().length).toEqual(3);
+  });
+});
